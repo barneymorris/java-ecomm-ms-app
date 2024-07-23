4337859c150dc4223812381a39ffc41b7a441cba
barneymorris <barneymorris.us@gmail.com>
2024-07-23 18:22:18 +0300

    add discovery server

---
+++ services/discovery-server/src/test/java/com/barneymorris/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barneymorris.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
