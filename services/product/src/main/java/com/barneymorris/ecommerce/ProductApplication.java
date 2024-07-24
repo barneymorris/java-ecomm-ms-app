7e490d969159fc3464aab666ace722d227c06a04
barneymorris <barneymorris.us@gmail.com>
2024-07-24 19:02:17 +0300

    more entityes

--- services/product/src/main/java/com/barneymorris/ecommerce/ProductApplication.java
+++ services/product/src/main/java/com/barneymorris/ecommerce/ProductApplication.java
@@ -1,4 +1,4 @@
-package com.barneymorris.ecomm;
+package com.barneymorris.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/product/src/test/java/com/barneymorris/ecommerce/ProductApplicationTests.java
+++ services/product/src/test/java/com/barneymorris/ecommerce/ProductApplicationTests.java
@@ -1,4 +1,4 @@
-package com.barneymorris.ecomm;
+package com.barneymorris.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
