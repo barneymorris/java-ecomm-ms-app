1fa5aeab9bbce4ec1a3f76fce5e503e28ce2f3b6
bsyrpin <bsyrpin@moysklad.ru>
2024-07-27 00:15:27 +0300

    create order ms

--- services/customer/src/main/java/com/barneymorris/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/barneymorris/ecommerce/customer/CustomerController.java
@@ -8,12 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
-
     private final CustomerService customerService;
-    private final CustomerMapper customerMapper;
 
     @PostMapping
     public ResponseEntity<String> createCustomer(
